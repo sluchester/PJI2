210f3f95dd86c2d4968c01b590cf62545c51aa4f
Jamilly da Silva Pinheiro <jamilly.p11@aluno.ifsc.edu.br>
2024-11-29 08:58:39 -0300

    Começando o código do sensor de luminosidade

--- hardware/src/LDR.cpp
+++ hardware/src/LDR.cpp
@@ -1,2 +1,16 @@
 //Fazer programação para o sensor LDR (Sensor de Luz)
 #include "LDR.h"
+
+const int LDR_PIN = 0; //Verificar qual a porta correta
+
+void setup(){
+    Serial.begin(9600);
+}
+
+void loop(){
+    int statusLuz = analogRead(LDR_PIN);
+
+    if(statusLuz == HIGH){
+        Serial.println("Apagada");
+    }else Serial.println("Ligada");
+}
--- hardware/src/LDR.h
+++ hardware/src/LDR.h
@@ -2,4 +2,25 @@
 #ifndef _LDR_H_
 #define _LDR_H_
 
+#include <Arduino.h>
+
+
+using namespace std;
+
+class LDR{
+    public:
+    LDR(int pin);
+
+    void setup();
+
+    void loop();
+
+    int getValor(); //Retorna o valor lido do LDR
+
+
+    private:
+  
+
+};
+
 #endif
